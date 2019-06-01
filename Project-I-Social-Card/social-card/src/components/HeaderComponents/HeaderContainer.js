d32e6993969aa5754338640f1ef123410824d221
dragonbegin <juan.reyes.mba@gmail.com>
2019-05-31 19:10:27 -0500

    HeaderContainer modify

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
 
+const HeaderContainer = () => {
+  return (
+    <div className = "container">
+      <div class = "theImg">
+        <ImageThumbnail />
+        <HeaderTitle />
+      </div>
+    </div>
+  )
+}
+
+export default HeaderContainer;
