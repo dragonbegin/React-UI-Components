d25f813760d6f6b0c2b8d928dfd11c6d273e9249
dragonbegin <juan.reyes.mba@gmail.com>
2019-05-31 19:42:08 -0500

    App.js modify

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,4 +1,18 @@
 import React from 'react';
 import './App.css';
+import CardContainer from './components/CardComponents/CardContainer';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import FooterContainer from './components/FooterComponents/Footer';
 
+const App = () => {
+  return (
+    <div className="app-container">
+      <HeaderContainer />
+      <CardContainer />
+      <FooterContainer/>
 
+    </div>
+  );
+};
+
+export default App;
