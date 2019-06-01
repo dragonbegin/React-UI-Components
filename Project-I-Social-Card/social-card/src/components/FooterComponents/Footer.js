3f5fb72beac6c4d2949fa43932d4b9ed84d7a101
dragonbegin <juan.reyes.mba@gmail.com>
2019-05-31 19:31:42 -0500

    Header.css and Footer.js modify

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,3 +2,15 @@ import React from 'react';
 import './Footer.css';
 
 
+const Footer = () => {
+    return(
+    <div class="footerDiv">
+    <p class="footerP">Box</p>
+    <p class="footerP">Box</p>
+        <p class="footerP">Box</p>
+        <p class="footerP">Box</p>
+    </div>
+    );
+};
+
+export default Footer;
